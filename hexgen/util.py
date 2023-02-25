0ecb74676fe02767765eb0e456aa2fcb967c1c51
xm <contact@techartserv.com>
2023-02-26 00:24:10 +0100

    Fix: colors should have int componentns not floats

--- hexgen/util.py
+++ hexgen/util.py
@@ -41,15 +41,15 @@ from itertools import combinations
 
 
 def blend_colors(color1, color2):
-    return min(round((color1[0] + color2[0]) / 2), 255), \
-        min(round((color1[1] + color2[1]) / 2), 255), \
-        min(round((color1[2] + color2[2]) / 2), 255)
+    return min(round((color1[0] + color2[0]) // 2), 255), \
+        min(round((color1[1] + color2[1]) // 2), 255), \
+        min(round((color1[2] + color2[2]) // 2), 255)
 
 
 def lighten(color, amount):
-    return min(round(color[0] + color[0] * amount), 255), \
-        min(round(color[1] + color[1] * amount), 255), \
-        min(round(color[2] + color[2] * amount), 255)
+    return min(math.trunc((round(color[0] + color[0] * amount)), 255), \
+        min(math.trunc((round(color[1] + color[1] * amount)), 255), \
+        min(math.trunc((round(color[2] + color[2] * amount)), 255)
 
 
 def randomize_color(color, dist=1):
